473727bbebeebcb27f734e4bc32983d2eadaaca5
VictorLoren <VictorLoren@users.noreply.github.com>
2015-11-21 15:30:52 -0800

    Update database with table definitions

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,40 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Once registered into a tournament, player keeps the same ID
+CREATE TABLE registeredPlayers (
+    id SERIAL primary key,
+    name TEXT
+ );
 
+-- Needed for multiple tournaments
+CREATE TABLE tournaments (
+    id SERIAL primary key,
+    name TEXT
+);
+
+-- Possibly not needed; could be combined into "tournaments" table
+CREATE TABLE playersInTournament (
+    player INTEGER references registeredPlayers (id),
+    tournament INTEGER references tournaments (id),
+    primary key (player, tournament)
+);
+
+-- Could be determined programmatically
+CREATE TABLE standings (
+    player INTEGER references registeredPlayers (id),
+    tournament INTEGER references tournaments (id),
+    points INTEGER DEFAULT 0,
+    standing INTEGER DEFAULT 0,
+    primary key (player, tournament)
+);
+
+-- Same number of matches for each round
+CREATE TABLE matches (
+    tournamentId INTEGER references tournaments,
+    roundNum INTEGER,
+    matchNum INTEGER,
+    winner INTEGER references registeredPlayers (id),
+    loser INTEGER references registeredPlayers (id),
+    primary key (tournamentId, roundNum, matchNum)
+);
